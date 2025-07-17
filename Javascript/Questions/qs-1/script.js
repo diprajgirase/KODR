76dadf017cbc397666361b93e870ebb1d027b1c4
Dipraj Girase <girasedipraj52@gmail.com>
2025-07-17 14:36:14 +0530

    commit

---
+++ Javascript/Questions/qs-1/script.js
@@ -0,0 +1,39 @@
+    let bookmarks = [];
+
+    // On page load, load existing bookmarks
+    window.onload = function() {
+        const saved = localStorage.getItem('bookmarks');
+        if (saved) {
+            bookmarks = JSON.parse(saved);
+            renderBookmarks();
+        }
+    };
+
+    function saveBookmark() {
+        const title = document.getElementById('titleInput').value;
+        const url = document.getElementById('urlInput').value;
+
+        if (!url.startsWith('http')) {
+            alert('URL must start with http or https');
+            return;
+        }
+
+        const bookmark = { title, url };
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+        renderBookmarks();
+
+        document.getElementById('titleInput').value = '';
+        document.getElementById('urlInput').value = '';
+    }
+
+    function renderBookmarks() {
+        const list = document.getElementById('bookmarkList');
+        list.innerHTML = '';
+        bookmarks.forEach(bm => {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="${bm.url}" target="_blank">${bm.title}</a>`;
+            list.appendChild(li);
+        });
+    }
