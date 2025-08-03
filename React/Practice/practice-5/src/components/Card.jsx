36f2988f8100fbae86f06f51f28e7f9e35669110
Dipraj Girase <girasedipraj52@gmail.com>
2025-08-03 23:18:16 +0530

    practice.log: 03/08

--- React/Practice/practice-5/src/components/Card.jsx
+++ React/Practice/practice-5/src/components/Card.jsx
@@ -4,6 +4,8 @@ const Card = () => {
   return (
     <div className=' bg-yellow-500 h-full w-screen' >
       <div className=' bg-pink-50 h-40 w-40' ></div>
+            <div className=' bg-pink-50 h-40 w-40' ></div>
+
     </div>
   )
 }
