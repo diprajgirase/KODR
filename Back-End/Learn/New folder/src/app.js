b2f868944cdc0d91eb6bd326782ba5637eb7adbb
Dipraj Girase <girasedipraj52@gmail.com>
2025-08-19 19:04:28 +0530

    practice log: 19/08

---
+++ Back-End/Learn/New folder/src/app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+module.exports = app
+
+// hA1IQ19BYLfSmfgb
+
+// mongodb+srv://Kodr-day2:hA1IQ19BYLfSmfgb@cluster0.h1r2wjz.mongodb.net/
