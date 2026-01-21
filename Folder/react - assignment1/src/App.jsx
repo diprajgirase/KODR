eb1c80fb346875ac16653386bc0f90b52e1c83d9
Dipraj Girase <girasedipraj52@gmail.com>
2026-01-21 14:45:59 +0530

    practice log

--- Folder/react - assignment1/src/App.jsx
+++ Folder/react - assignment1/src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  return (
-    <>
-    useEffect(() => {
-       fetch("https://jsonplaceholder.typicode.com/posts")
-           .then((res) => res.json())
-           .then((data)=> {
-            setData(data)
-            setLoading(false)
-           })
-           .catch((err)=>{
-            setError(err)
-            setLoading(false)
-           })
-    }, [])
-    </>
-  )
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data)
+        setLoading(false);
+      })
+      .catch((err)=>{
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return <></>;
 }
 
 export default App;
